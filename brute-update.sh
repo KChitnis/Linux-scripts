c0d459bb8e47bb4e84a9abd53a11bbdca8bc00ee
MorphTail <47940274+m0rphtail@users.noreply.github.com>
2020-05-19 10:12:59 +0530

    Update brute-update.sh

--- brute-update.sh
+++ brute-update.sh
@@ -1,6 +1,8 @@
+#script to update/ download if your internet is slow
+
 #!/bin/sh
-INPUT_STRING=hello
-while [ "$INPUT_STRING" != "bye" ]
+INPUT_STRING=yes
+while [ "$INPUT_STRING" != "no" ]
 do
-  sudo eopkg up -y && INPUT_STRING=bye
+  sudo eopkg up -y && INPUT_STRING=no
 done
